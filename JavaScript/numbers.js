49f62c881c0820ef0eaf9eb1c1a94d104ad0e15a
Irakli DT <120053811+IrakliDT@users.noreply.github.com>
2023-02-23 23:58:24 +0400

    Delete numbers.js

--- JavaScript/numbers.js
+++
@@ -1,9 +0,0 @@
-var numbers =   ["1","4","18","24","15","2","3","19","55"]
-console.log(numbers[0])
-console.log(numbers[4])
-console.log(numbers[6])
-console.log(numbers[7])
-console.log(numbers[8])
-
-
- 
